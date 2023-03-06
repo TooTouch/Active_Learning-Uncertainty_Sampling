989ca7bf1c4507f45a788b7e79f7de151af7da20
tootouch <wogur379@gmail.com>
2023-03-06 14:35:13 +0900

    [query] update random sampling

--- query_strategies/random.py
+++ query_strategies/random.py
@@ -7,13 +7,7 @@ def random_sampling(
     
     # set unlabeled index
     unlabeled_idx = np.arange(len(dataset))[~labeled_idx]
-    
-    # extract unlabeled probs
-    probs = extract_unlabeled_prob(model=model, dataloader=dataloader, dataset=dataset, unlabeled_idx=unlabeled_idx)
-    
-    # select random select
-    max_confidence = probs.max(1)[0]
-    shuffled_idx = torch.randperm(len(max_confidence))
-    select_idx = unlabeled_idx[shuffled_idx[:nb_query_samples]]
+    np.random.shuffle(unlabeled_idx)
+    select_idx = unlabeled_idx[:nb_query_samples]
     
     return select_idx
